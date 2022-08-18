7cdd522915925d601f102d267445318af47b2f30
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-08-18 18:50:06 +0200

    Init yoo 🤓🤍

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("java")
+    id("org.jetbrains.intellij") version "1.6.0"
+}
+
+group = "com.anas.intellij.plugins.ayah"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    implementation("com.github.anas-elgarhy:alquran-cloud-api:0.4.0-v1")
+}
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    version.set("2021.3")
+    type.set("IC") // Target IDE Platform
+
+    plugins.set(listOf(/* Plugin Dependencies */))
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("213")
+        untilBuild.set("223.*")
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
