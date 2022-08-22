decfa296283bd0624fdf723990dceabec57f015d
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-08-22 14:13:57 +0200

    😆 Close the player when closing the details dialog 💙

--- src/main/java/com/anas/intellij/plugins/ayah/dialogs/AyahDetailsDialog.java
+++ src/main/java/com/anas/intellij/plugins/ayah/dialogs/AyahDetailsDialog.java
@@ -87,20 +87,28 @@ public class AyahDetailsDialog extends JDialog implements PlayerListener {
             }
         });
 
-        buttonCancel.addActionListener(l -> dispose());
+        buttonCancel.addActionListener(l -> close());
 
         // call onCancel() when cross is clicked
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
-                dispose();
+                close();
             }
         });
 
         // call onCancel() on ESCAPE
-        contentPane.registerKeyboardAction(l -> dispose(),
+        contentPane.registerKeyboardAction(l -> close(),
                 KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
                 JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    }
+
+    private void close() {
+        if (isPlaying) {
+            audioPlayer.stop();
+            audioPlayer = null;
+        }
+        dispose();
     }
 
     private void playOrNot() {
